29b482447de18edb445a7189e51bea6e3921a8a9
David Liebherr <ambition.aidev@gmail.com>
2024-06-19 17:30:58 -0500

    Just making a commit just becuase I have jus been playing around with nothing importian to commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = 3001;
 
-console.log("Stacey is fucking hott")
+console.log("Stacey is fucking hott");
 
 // HTML or View routes
 app.get("/about", (req, res) => {
@@ -11,7 +11,7 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "contact.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/contact", (req, res) => {
@@ -19,10 +19,14 @@ app.get("/contact", (req, res) => {
 });
 
 // api routes
-const pets = require('./data/pets.json') // path for imported data
-app.get('/api/search-pets', (req, res) => {
-   res.json(pets)
-})
+const stuff = require("./data/stuff.json"); // path for imported data
+app.get("/getstuff", (req, res) => {
+const q = req.query.name
+const result_of_q_filtered = stuff.filter(something => something.name === q)
+res.json(result_of_q_filtered)
+});
+
+
 
 app.listen(port, () => {
   console.log(`Express running on http://localhost:${port}`);
